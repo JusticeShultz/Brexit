fe6bf19c1d2b3f23ab4210731a0a2db74a993115
Bread <redatonedash@gmail.com>
2019-01-17 14:26:10 -0800

    added jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,19 +16,25 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, out hit, 1) && transform.GetComponent<Rigidbody>().velocity.y <= 0) { //Grounded
+        if (Physics.Raycast(transform.position + new Vector3(0,1,0), -transform.up, out hit, 1) && transform.GetComponent<Rigidbody>().velocity.y <= 0) { //Grounded
+            Debug.DrawRay(transform.position, -transform.up, Color.yellow);
             state = State.Idle;
             //if()
             inAir = false;
-            transform.position = new Vector3(transform.position.x, hit.point.y + 0.999f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
             transform.GetComponent<Rigidbody>().velocity = new Vector3(transform.GetComponent<Rigidbody>().velocity.x * 0.95f, transform.GetComponent<Rigidbody>().velocity.y, 0);
-            if (Input.GetAxis("Vertical" + PlayerNumber) < -0.5) {
+            if (Input.GetAxis("Vertical" + PlayerNumber) < -0.5) { //jump
+                transform.GetComponent<Rigidbody>().velocity = transform.GetComponent<Rigidbody>().velocity + new Vector3(0,10,0);
+            } else if (Input.GetAxis("Vertical" + PlayerNumber) > 0.5) {
                 state = State.Guard;
+                transform.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                print("lmao");
             }  else if (Input.GetAxis("Horizontal" + PlayerNumber) > 0) {
-                transform.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal" + PlayerNumber) * 5, 0, 0);
-            } else if (Input.GetAxis("Horizontal" + PlayerNumber) < 0)
-            {
-                transform.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal" + PlayerNumber) * 5, 0, 0);
+                transform.eulerAngles = new Vector3(0, 90, 0);
+                transform.GetComponent<Rigidbody>().velocity = new Vector3(Mathf.Max(Input.GetAxis("Horizontal" + PlayerNumber) * 5, transform.GetComponent<Rigidbody>().velocity.x), 0, 0);
+            } else if (Input.GetAxis("Horizontal" + PlayerNumber) < 0) {
+                transform.eulerAngles = new Vector3(0, -90, 0);
+                transform.GetComponent<Rigidbody>().velocity = new Vector3(Mathf.Min(Input.GetAxis("Horizontal" + PlayerNumber) * 5, transform.GetComponent<Rigidbody>().velocity.x), 0, 0);
             } else {
                 transform.GetComponent<Rigidbody>().velocity = new Vector3(transform.GetComponent<Rigidbody>().velocity.x, 0, 0);
             }
@@ -36,6 +42,8 @@ public class PlayerController : MonoBehaviour {
 
         } else {
             inAir = true;
+            Vector3 wishdir = new Vector3(Input.GetAxis("Horizontal" + PlayerNumber), Mathf.Max(Input.GetAxis("Vertical" + PlayerNumber),0)*-6,0)*Time.deltaTime*10;
+            transform.GetComponent<Rigidbody>().velocity += wishdir;
         }
     }
 }
