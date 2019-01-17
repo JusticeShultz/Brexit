98c001c70587f693d1ae7b32193b22d38598c828
amandaevans18 <42452129+amandaevans18@users.noreply.github.com>
2019-01-17 12:37:09 -0800

    I made it!

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+
+    public void changeScene(string targetScene)
+    {
+        Debug.Log("Loading Scene");
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void exitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
