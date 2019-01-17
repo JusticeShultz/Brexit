d69a92d49ce1e81779692a7a9f86b79d620130d1
Justice Shultz <shultzjc1@gmail.com>
2019-01-16 18:25:28 -0800

    Last minute art additions

---
+++ Assets/Scripts/Windmill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windmill : MonoBehaviour {
+
+    Transform transform = GetComponent<Transform>();
+
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+	
+	void Update ()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime);
+    }
+}
