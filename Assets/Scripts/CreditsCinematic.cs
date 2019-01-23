6a02706e73b94616ebd2f39cc857452879297532
Justice Shultz <shultzjc1@gmail.com>
2019-01-23 15:23:44 -0800

    Credits screen no longer broken

---
+++ Assets/Scripts/CreditsCinematic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsCinematic : MonoBehaviour
+{
+    public float speed = 30f;
+    void Update()
+    {
+        transform.localRotation *= Quaternion.AngleAxis(speed * Time.deltaTime, Vector3.up);
+    }
+}
