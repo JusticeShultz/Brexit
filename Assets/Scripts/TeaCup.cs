c1254758980fd2d26db1874ca1e3876a3c60adaa
Bread <redatonedash@gmail.com>
2019-01-23 16:50:28 -0800

    TeaCup

---
+++ Assets/Scripts/TeaCup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeaCup : MonoBehaviour {
+    Rigidbody rg;
+	// Use this for initialization
+	void Start () {
+        rg = transform.GetComponent<Rigidbody>();
+        Yeet(new Vector2(10, 10));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Yeet(Vector2 dir) {
+        rg.isKinematic = false;
+        rg.velocity = new Vector3(dir.x, dir.y);
+        
+    }
+    void OnCollisionEnter() {
+
+    }
+}
