8f086ce0144ce284ab3addc5f99df573e3fc820d
Justice Shultz <shultzjc1@gmail.com>
2019-01-24 12:19:50 -0800

    merge

---
+++ Assets/Scripts/DontCollideWithPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontCollideWithPlayer : MonoBehaviour
+{
+    GameObject P1;
+    GameObject P2;
+
+    void Start ()
+    {
+        P1 = GameObject.Find("Player1");
+        P2 = GameObject.Find("Player2");
+        Physics.IgnoreCollision(GetComponent<Collider>(), P1.GetComponent<Collider>());
+        Physics.IgnoreCollision(GetComponent<Collider>(), P2.GetComponent<Collider>());
+    }
+}
