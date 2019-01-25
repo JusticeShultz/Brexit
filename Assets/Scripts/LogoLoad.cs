9fe3fb14700f1d10b707bd60d288abc876e7dd98
Justice Shultz <shultzjc1@gmail.com>
2019-01-24 17:50:32 -0800

    Finished!

---
+++ Assets/Scripts/LogoLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoLoad : MonoBehaviour
+{
+    public GameObject Sound;
+    int Frame = 0;
+
+    void Update ()
+    {
+        ++Frame;
+        
+        if(Frame >= 200)
+        {
+            Sound.SetActive(true);
+        }
+	}
+}
